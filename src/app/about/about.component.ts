e5e87e2be1bcc937346e04f4e002f5a73194ce70
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-28 17:39:37 -0500

    Adding comments below code on observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { createObservable} from '../common/util';
 import {
   concat,
   fromEvent,
@@ -14,7 +15,8 @@ import {
   AsyncSubject,
   ReplaySubject
 } from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
+import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
+import { json } from 'body-parser';
 
 @Component({
   selector: 'about',
@@ -26,17 +28,80 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-   const interval$ = timer(1000,1000);
-   const sub = interval$.subscribe((val) => console.log(val));
+    const http$ = createObservable('/api/courses');
+    // pipes the http observable into the map function rxjs to call a method on all its values returning and array of the response objects property payload
+    const courses$ = http$
+      .pipe(
+        map(res =>  Object.values(res["payload"]) )
+      );
 
-   setTimeout(() => sub.unsubscribe(), 5000);
+    // now subscribe to the courses observable
+    courses$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    )
+    
+
+
+    // ========= end of ngOnInit
   }
 
 }
 
-//  the dollar sign = type rxjs Observable the observable functions are blueprints for streams stored in the variables 
-// We assign the interval subscribe method to const sub and the call setTimeout on it ...
-  //  const interval$ = timer(1000,1000);
-  //  const sub = interval$.subscribe((val) => console.log(val));
 
-  //  setTimeout(() => sub.unsubscribe(), 5000);
+
+
+// ======== Note 1 ====
+// The dollar sign = type rxjs Observable the observable functions are blueprints for streams stored in the variables 
+// We assign the interval subscribe method to const sub and the call setTimeout on it ...
+
+//  const interval$ = timer(1000,1000);
+//  const sub = interval$.subscribe((val) => console.log(val));
+//  setTimeout(() => sub.unsubscribe(), 5000);
+// =========== end of note 1 ======
+
+//========================== note 2 ====
+// Object.create() method to create custom observable  rxjs lib
+//  will create a http$ service to hit the api end point api/courses 
+//  Observable.create(observer => {
+//   three methods abailable 
+//   1. observer.complete()
+//   2. oberver.next(); can call multiple times 
+//   3. observer.error()
+
+// })
+
+// ======= code below shows how to hit an api creating you own observable  ==== console.logs data from endpoint
+
+// const http$ = Observable.create(observer => {
+//   fetch('api/courses')
+//     .then(data => {
+//       return data.json();
+//     })
+//     .then( response => {
+//       observer.next(response);
+//       observer.complete()
+//     })
+//     .catch(err => {
+//       observer.error(err);
+//     })
+// });
+
+// http$.subscribe(data => {
+//  console.log(data.payload.forEach(val => {
+//    console.log(val)
+//  }))
+// })
+// }
+
+// ========================= note on creating an observable to another format by piping the result into the map method of rxjs lib
+
+// const http$ = createObservable('/api/courses');
+//     // pipes the http observable into the map function rxjs to call a method on all its values returning and array of the response objects property payload
+//     const courses$ = http$.pipe(
+//       map(res => {
+//         Object.values(res["payload"])
+//       })
+//     )
+    
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,18 @@
+import {Observable } from 'rxjs';
 
+export function createObservable(url: string) {
+    return Observable.create(observer => {
+     fetch(url)
+       .then(data => {
+         return data.json();
+       })
+       .then( response => {
+         observer.next(response);
+         observer.complete()
+       })
+       .catch(err => {
+         observer.error(err);
+       })
+   });
 
+ }
